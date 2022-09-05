e85d34e378daf091dd077e41380f7f1dde03fd7c
Kerim <kerimcetintas@gmail.com>
2022-09-05 11:55:35 +0300

    feat: add `breakpointAliases` into `createTheme` utility

--- packages/core/src/theme/createTheme.ts
+++ packages/core/src/theme/createTheme.ts
@@ -1,5 +1,47 @@
 import { DesygnaTheme } from "./types";
 
-export function createTheme(theme: DesygnaTheme): DesygnaTheme {
-  return theme;
+export type CreateThemeType = DesygnaTheme & {
+  breakpointAliases?: string[];
+};
+
+export function createTheme({
+  breakpointAliases,
+  breakpoints,
+  borderRadius,
+  borderWidth,
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  letterSpacings,
+  lineHeights,
+  sizes,
+  shadows,
+  space,
+  zIndices
+}: CreateThemeType): DesygnaTheme {
+  if (breakpointAliases) {
+    breakpointAliases.forEach((alias, index) => {
+      // @ts-ignore
+      breakpoints[alias] = breakpoints[index];
+    });
+  }
+
+  const t: DesygnaTheme = {
+    breakpoints,
+    borderRadius,
+    borderWidth,
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    letterSpacings,
+    lineHeights,
+    sizes,
+    shadows,
+    space,
+    zIndices
+  };
+
+  return t;
 }
