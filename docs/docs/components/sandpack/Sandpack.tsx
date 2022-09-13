640c30941ab05d5d6a5137f654833ec130dd212b
Kerim <kerimcetintas@gmail.com>
2022-09-13 22:22:33 +0300

    docs: add `sandpack` with minimal setup

---
+++ docs/docs/components/sandpack/Sandpack.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Sandpack } from "@codesandbox/sandpack-react";
+import { monokaiPro } from "@codesandbox/sandpack-themes";
+import { rootCode } from "./rootCode";
+import { desygnaVersion } from "@site/docs/desygnaVersion";
+
+export interface CommonSandpackProps {
+  files?: Record<string, string>;
+  deps?: Record<string, string>;
+  editorHeight?: number;
+}
+
+export const CommonSandpack = ({
+  files,
+  deps,
+  editorHeight = 800
+}: CommonSandpackProps) => {
+  return (
+    <Sandpack
+      theme={monokaiPro}
+      template="react-ts"
+      files={{
+        "/index.tsx": rootCode,
+        ...files
+      }}
+      customSetup={{
+        dependencies: {
+          react: "17.0.2",
+          "react-dom": "17.0.2",
+          "react-scripts": "4.0.3",
+          "@emotion/react": "^11.10.0",
+          "@emotion/styled": "^11.10.0",
+          "@desygna/desygna": desygnaVersion,
+          ...deps
+        }
+      }}
+      options={{
+        showLineNumbers: true,
+        showInlineErrors: true,
+        editorHeight
+      }}
+    />
+  );
+};
