db1626e8e936614621c9ba3c0953e1dfb3b88e6b
Kerim <kerimcetintas@gmail.com>
2022-09-17 18:22:40 +0300

    fix: `color` system

--- packages/core/src/system/color.ts
+++ packages/core/src/system/color.ts
@@ -1,13 +1,1 @@
-import * as CSS from "csstype";
-import { system } from "styled-system";
-
-export type ColorProps = {
-  color?: CSS.Properties["color"];
-};
-
-export const color = system({
-  color: {
-    scale: "colors",
-    property: "color"
-  }
-});
+export { color, ColorProps } from "styled-system";
