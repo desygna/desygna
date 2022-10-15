68aa22a715ed3675fca89aa5f8e711ca99c14f3e
Kerim <kerimcetintas@gmail.com>
2022-10-15 17:11:48 +0300

    Export new components

--- packages/desygna/src/exports.ts
+++ packages/desygna/src/exports.ts
@@ -1,4 +1,10 @@
+// Core
 export * from "@desygna/desygna-core";
-
-export * from "@desygna/desygna-box";
+// Components
+export * from "@desygna/desygna-absolute";
 export * from "@desygna/desygna-button";
+export * from "@desygna/desygna-box";
+export * from "@desygna/desygna-flex";
+export * from "@desygna/desygna-grid";
+export * from "@desygna/desygna-paragraph";
+export * from "@desygna/desygna-span";
