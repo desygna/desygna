372b353ab2cd6bc5a3dcee47c83842ec7ab854c7
Kerim <kerimcetintas@gmail.com>
2022-10-15 16:07:22 +0300

    feat: add `grid` component

---
+++ packages/components/flex/src/flex.ts
@@ -0,0 +1,14 @@
+import { HTMLAttributes } from "react";
+import {
+  styled,
+  AsProp,
+  FlexboxProps,
+  flexbox,
+  compose
+} from "@desygna/desygna-core";
+
+export type FlexComponentProps = HTMLAttributes<HTMLDivElement> &
+  FlexboxProps &
+  AsProp;
+
+export const Flex = styled.div<FlexComponentProps>(compose(flexbox));
