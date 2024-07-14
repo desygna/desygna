b87131510ed33e0da4fb5ac8f3791a163dcbec84
Kerim <41960283+kerimcet@users.noreply.github.com>
2024-07-15 00:57:12 +0300

    Revert "Refactor `createDesygnaTheme`"
    This reverts commit 8d17eaae8d1cf57ad5fa0865f21bcaeb3da256bd.

--- packages/core/src/theme/createDesygnaTheme.ts
+++ packages/core/src/theme/createDesygnaTheme.ts
@@ -1,29 +1,7 @@
 import { DesygnaTheme } from "./types";
 
-type Bps<T = string, K = Record<string, string>> = T[] & {
-  [P in keyof K]?: string;
-};
-
-function isValidObject(value: any): value is Record<string, string> {
-  return typeof value === "object" && value !== null && !Array.isArray(value);
-}
-
-export function createDesygnaTheme<T extends Partial<DesygnaTheme> = Partial<DesygnaTheme>>(
-  theme: T
-): T {
-  let breakpoints: Bps<string, T["breakpoints"]> = [] as Bps<string, T["breakpoints"]>;
-
-  if (!isValidObject(theme.breakpoints)) {
-    console.warn("[Desygna] - Please provide a valid `breakpoints` for theme");
-    return theme;
-  }
-
-  if (theme.breakpoints) {
-    Object.entries(theme.breakpoints).forEach(([key, value]) => {
-      (breakpoints as any)[key] = value;
-    });
-  }
-
-  theme.breakpoints = breakpoints;
+export function createDesygnaTheme<
+  T extends Partial<DesygnaTheme> = Partial<DesygnaTheme>
+>(theme: T): T {
   return theme;
 }
